bd23d04867f070eb8f1da1c53c0ea181774b173f
cktan105-git <62925392+cktan105-git@users.noreply.github.com>
2025-06-11 11:17:44 +0800

    - Quick fix in CsvContentParser to handle line endings from different OS

--- src/CodingAssignmentLib/CsvContentParser.cs
+++ src/CodingAssignmentLib/CsvContentParser.cs
@@ -7,10 +7,12 @@ public class CsvContentParser : IContentParser
     // TODO: To check if could use a library like CsvHelper or similar so to handle CSV parsing more robustly, e.g. following RFC 4180
     public IEnumerable<Data> Parse(string content)
     {
-        return content.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries).Select(line =>
-        {
-            var items = line.Split(',', StringSplitOptions.RemoveEmptyEntries);
-            return new Data(items[0], items[1]);
-        });
+        return content.ReplaceLineEndings() // Quick fix: Normalize line endings to handle different OS formats
+                      .Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries)
+                      .Select(line =>
+                      {
+                          var items = line.Split(',', StringSplitOptions.RemoveEmptyEntries);
+                          return new Data(items[0], items[1]);
+                      });
     }
 }
